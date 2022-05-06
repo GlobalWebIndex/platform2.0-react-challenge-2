fd57bfa8b22423092f8dc3192665af1e1605e70a
Kaplya <kaplyak@protonmail.com>
2022-05-06 12:49:53 +0200

    Project setup w/ dependencies. Styled components, React Router.

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return(
+        <div>
+            <div>
+                <a href="/">Random dog image</a>
+                <a href="/">Dog breeds</a>
+            </div>
+        </div>
+    )
+}
